3a71efca092911153b775cc6eb319011228c65e6
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2023-12-13 10:50:56 +0530

    add firebase tools

---
+++ src/config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBdNUG_iw7qMVQblWdFXmMr59Arqssjyo",
+  authDomain: "react-firebase-9e6e0.firebaseapp.com",
+  projectId: "react-firebase-9e6e0",
+  storageBucket: "react-firebase-9e6e0.appspot.com",
+  messagingSenderId: "207143627408",
+  appId: "1:207143627408:web:54d1aa0aeee0b9b6680615",
+  measurementId: "G-119DDKWXS6"
+};
+
+const app = initializeApp(firebaseConfig);
